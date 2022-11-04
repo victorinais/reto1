cb9f6ebee0322ecead589660a25fcaa421e150a1
victorinais <vicmarin50@gmail.com>
2022-11-04 11:05:21 -0500

    se agrega opciones de moneda, se da funcionalidad, se cambia color de botones

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:html';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         
@@ -33,6 +35,9 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+final textorigen = TextEditingController();
+final textdestino = TextEditingController();
+
 class objetos {
   final titulo;
   final icono;
@@ -40,23 +45,40 @@ class objetos {
 
   objetos(this.titulo, this.icono, this.color);
 }
+String op1 = "USD";
+String op2 = "COP";
+
+List<DropdownMenuItem<String>> monedas = <DropdownMenuItem<String>>[
+  DropdownMenuItem(
+    child: Text("USD"),
+    value: "USD",
+    ),
+    DropdownMenuItem(
+    child: Text("EUR"),
+    value: "EUR",
+    ),
+    DropdownMenuItem(
+    child: Text("COP"),
+    value: "COP",
+    ),
+];
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
 List<objetos> lista = <objetos>[
-  objetos("7", Icon(Icons.delete), Color.fromARGB(255, 99, 97, 97)),
-  objetos("8", Icon(Icons.delete), Color.fromARGB(255, 99, 97, 97)),
-  objetos("9", Icon(Icons.delete), Color.fromARGB(255, 99, 97, 97)),
-  objetos("4", Icon(Icons.delete), Color.fromARGB(255, 99, 97, 97)),
-  objetos("5", Icon(Icons.delete), Color.fromARGB(255, 99, 97, 97)),
-  objetos("6", Icon(Icons.delete), Color.fromARGB(255, 99, 97, 97)),
-  objetos("1", Icon(Icons.delete), Color.fromARGB(255, 99, 97, 97)),
-  objetos("2", Icon(Icons.delete), Color.fromARGB(255, 99, 97, 97)),
-  objetos("3", Icon(Icons.delete), Color.fromARGB(255, 99, 97, 97)),
-  objetos("Borrar", Icon(Icons.delete), Color.fromARGB(255, 99, 97, 97)),
-  objetos("0", Icon(Icons.delete), Color.fromARGB(255, 99, 97, 97)),
-  objetos("Convertir", Icon(Icons.delete), Color.fromARGB(255, 99, 97, 97)),
+  objetos("9", Icon(Icons.abc), Color.fromARGB(255, 177, 167, 167)),
+  objetos("8", Icon(Icons.abc), Color.fromARGB(255, 177, 167, 167)),
+  objetos("7", Icon(Icons.abc), Color.fromARGB(255, 177, 167, 167)),
+  objetos("6", Icon(Icons.abc), Color.fromARGB(255, 177, 167, 167)),
+  objetos("5", Icon(Icons.abc), Color.fromARGB(255, 177, 167, 167)),
+  objetos("4", Icon(Icons.abc), Color.fromARGB(255, 177, 167, 167)),
+  objetos("3", Icon(Icons.abc), Color.fromARGB(255, 177, 167, 167)),
+  objetos("2", Icon(Icons.abc), Color.fromARGB(255, 177, 167, 167)),
+  objetos("1", Icon(Icons.abc), Color.fromARGB(255, 177, 167, 167)),
+  objetos("0", Icon(Icons.abc), Color.fromARGB(255, 177, 167, 167)),
+  objetos("Borrar", Icon(Icons.delete), Color.fromARGB(255, 177, 167, 167)),
+  objetos("Convertir", Icon(Icons.calculate), Color.fromARGB(255, 177, 167, 167)),
 ];
 
 
@@ -77,7 +99,38 @@ List<objetos> lista = <objetos>[
       ),
       body: Padding(
         padding: EdgeInsets.all(15),
-        child: GridView.builder(
+        child: Column(
+          children: [
+            Row(children: [
+              Text("origen"),
+              DropdownButton(
+                value: op1,
+                items: monedas, onChanged: (String? x) {
+                  setState(() {
+                    op1 = x.toString();
+                  });
+                }),
+            Row(children: [
+              Text("destino"),
+              DropdownButton(
+                value: op2,
+                items: monedas, onChanged: (String? x) {
+                  setState(() {
+                    op2 = x.toString();
+                  });
+                }),
+            ]),
+            ]),
+            TextField(
+              controller: textorigen,
+              decoration: InputDecoration(labelText: "origen", hintText: "0"),
+            ),
+            TextField(
+              controller: textdestino,
+              decoration: InputDecoration(labelText: "destino", hintText: "0"),
+            ),
+            Expanded(
+              child: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
         ),
@@ -87,12 +140,57 @@ List<objetos> lista = <objetos>[
             color: lista[index].color,
             child: ListTile(
               title: Center(
-                child: Text(lista[index].titulo),
+                child: index > 9
+                ? lista[index].icono
+                : Text(lista[index].titulo,
+                style: TextStyle(
+                  fontSize: 30, fontWeight: FontWeight.bold),),
                   ),
+                  onTap: () {
+                    print(lista[index].titulo);
+
+                    if (index < 10) {
+                      textorigen.text = textorigen.text + lista[index].titulo;
+                    } else if (index == 10) {
+                      textorigen.text = "";
+                      textdestino.text = "";
+                    } else {
+                      if (op1 == "USD" && op2 == "COP"){
+                        textdestino.text = 
+                        (double.parse(textorigen.text) * 5018)
+                        .toString();
+                      } else if (op1 == "COP" && op2 == "USD"){
+                        textdestino.text = 
+                        (double.parse(textorigen.text) / 5018)
+                        .toString();
+                      } else if (op1 == "EUR" && op2 == "COP"){
+                        textdestino.text = 
+                        (double.parse(textorigen.text) * 4965)
+                        .toString();
+                      } else if (op1 == "COP" && op2 == "EUR"){
+                        textdestino.text = 
+                        (double.parse(textorigen.text) / 4965)
+                        .toString();
+                      } else if (op1 == "EUR" && op2 == "USD"){
+                        textdestino.text = 
+                        (double.parse(textorigen.text) * 0.99)
+                        .toString();
+                      } else if (op1 == "USD" && op2 == "EUR"){
+                        textdestino.text = 
+                        (double.parse(textorigen.text) / 0.99)
+                        .toString();
+                      } else {
+                        textdestino.text = textdestino.text;
+                      }
+                    }
+                    
+                  },
                 ),
               );
             },
-          ),
+          ),)
+          ],
+        )
         )
     );
   }
